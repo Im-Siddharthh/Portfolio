f1998663a1c258ba8938833162287906bfd8e721
Im-Siddharthh <86145528+Im-Siddharthh@users.noreply.github.com>
2024-04-15 23:51:13 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-  strings: ["Software Development", "C++ Programming", "web development"],
+  strings: ["Software Development", "C++ Programming", "Script Writing"],
   loop: true,
   typeSpeed: 50,
   backSpeed: 25,
